ab7e72ec8abaa32b232a58d4e803e6e17c6559c1
Sivashankari@brillersys <55868182+SivashankariVaitheswaran@users.noreply.github.com>
2019-11-22 17:20:47 +0530

    Add files via upload

---
+++ springbootdemo/springbootdemo/src/main/java/com/employee/demo/springbootdemo/Employee.java
@@ -0,0 +1,35 @@
+package com.employee.demo.springbootdemo;
+
+public class Employee {
+
+    public Employee() {
+
+    }
+
+    public Employee(Integer id, String firstName, String lastName, String email) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    //Getters and setters
+
+    @Override
+    public String toString() {
+        return ("Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]");
+    }
+
+    public void setId(Integer id) {
+    }
+
+    public Object getId() {
+        return id;
+    }
+}
---
+++ springbootdemo/springbootdemo/src/test/java/com/employee/demo/springbootdemo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.employee.demo.springbootdemo;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
